93d26a84576c4d8ce49f440d1b1166a01b9f258c
jaekyunahn <ajg10795@naver.com>
2020-08-01 01:39:14 +0900

    rework main.c

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char ** argv)
 {
+	char sData[128] = "";
+	
+	memset( sData, 0x00, strlen(sData));
+	
 	printf("Hello world\r\n");
 	return 0;
 }
